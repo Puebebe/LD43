85c7042c6014dfefd43e2e6f21703eeff695df7d
Bartosz Jakubowski <bjakubowski.dev@gmail.co>
2018-12-03 18:47:47 +0100

    Add music player

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	[SerializeField] public AudioClip[] clips;
+	private AudioSource audioSource;
+
+	void Start () {
+		audioSource = FindObjectOfType<AudioSource>();
+		audioSource.loop = false;
+	}
+
+	private AudioClip GetRandomClip(){
+		return clips[Random.Range(0, clips.Length)];
+	}
+
+	void Update () {
+		if (!audioSource.isPlaying)
+		{
+			audioSource.clip = GetRandomClip();
+			audioSource.Play();
+		}
+
+	}
+}
